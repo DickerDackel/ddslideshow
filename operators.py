27e09ca64e474631b7e9fb04d27e02dfcd039e82
Michael Lamertz <michael.lamertz@gmail.com>
2024-08-10 19:34:24 +0200

    * Run all

--- operators.py
+++ operators.py
@@ -11,36 +11,66 @@ from .utils import create_transform, crossfade, grep, load_image, apply_zoom
 OP_PREFIX = 'ddslideshow'
 
 
-class SEQUENCE_EDITOR_OT_create_slideshow(Operator):
+def shit_i_cannot_figure_out_how_to_properly_call_load_images__from_create_slideshow(file_collection, directory, context):
+    scene = context.scene
+    ddslideshow = scene.ddslideshow
+    fps = context.scene.render.fps
+
+    intro = bpy.path.abspath(ddslideshow.intro)
+    outro = bpy.path.abspath(ddslideshow.outro)
+    slide_duration = int(ddslideshow.slide_duration * fps)
+    slide_crossfade = int(ddslideshow.slide_crossfade * fps)
+
+    files = [os.path.join(directory, f.name) for f in file_collection]
+
+    if os.path.isfile(intro):
+        files.insert(0, intro)  # noqa: E701
+
+    if os.path.isfile(outro):
+        files.append(outro)  # noqa: E701
+
+    full_duration = slide_duration + 2 * slide_crossfade
+    images = [load_image(bpy.path.abspath(fname), 1 + i * full_duration, full_duration)
+              for i, fname in enumerate(files)]
+
+    context.scene.frame_end = images[-1].frame_final_end
+
+
+class SEQUENCE_EDITOR_OT_create_slideshow(Operator, ImportHelper):
     bl_idname = f'{OP_PREFIX}.create_slideshow'
     bl_label = 'Run workflow'
+
+    files: CollectionProperty(name="File Path", type=OperatorFileListElement)
+    directory: StringProperty(subtype='DIR_PATH', default='//')
+    filename_ext = ""
+    filter_glob: StringProperty(default='*.jpg;*.png;*.tif;*.tiff;*.bmp')
 
     def execute(self, context):
         def set_select(sequence, value):
             for strip in sequence:
                 strip.select = value
 
-        # bpy.ops.ddslideshow.load_images()
+        shit_i_cannot_figure_out_how_to_properly_call_load_images__from_create_slideshow(self.files, self.directory, context)
+
         set_select(bpy.context.scene.sequence_editor.sequences, False)
         set_select(grep(bpy.context.scene.sequence_editor.sequences, 'IMAGE'), True)
-        bpy.ops.ddslideshow.overlap_images()
-        bpy.ops.ddslideshow.add_transforms()
+        bpy.ops.ddslideshow.overlap_images('INVOKE_DEFAULT')
+        bpy.ops.ddslideshow.add_transforms('INVOKE_DEFAULT')
 
         set_select(bpy.context.scene.sequence_editor.sequences, False)
         set_select(grep(bpy.context.scene.sequence_editor.sequences, 'TRANSFORM'), True)
-        bpy.ops.ddslideshow.zoom_transforms()
-        # bpy.ops.ddslideshow.pan_transforms()
+        bpy.ops.ddslideshow.zoom_transforms('INVOKE_DEFAULT')
+        # bpy.ops.ddslideshow.pan_transforms('INVOKE_DEFAULT')
 
         set_select(bpy.context.scene.sequence_editor.sequences, False)
         set_select(grep(bpy.context.scene.sequence_editor.sequences, 'TRANSFORM'), True)
-        bpy.ops.ddslideshow.crossfade()
+        bpy.ops.ddslideshow.crossfade('INVOKE_DEFAULT')
 
         set_select(bpy.context.scene.sequence_editor.sequences, False)
-        bpy.ops.ddslideshow.slideshow_fade_in_out()
-        bpy.ops.ddslideshow.audio_fade_in_out()
+        bpy.ops.ddslideshow.slideshow_fade_in_out('INVOKE_DEFAULT')
+        bpy.ops.ddslideshow.audio_fade_in_out('INVOKE_DEFAULT')
 
         return {'FINISHED'}
-
 
 class SEQUENCE_EDITOR_OT_load_images(Operator, ImportHelper):
     bl_idname = f'{OP_PREFIX}.load_images'
@@ -49,32 +79,12 @@ class SEQUENCE_EDITOR_OT_load_images(Operator, ImportHelper):
 
     # Configure ImportHelper
     files: CollectionProperty(name="File Path", type=OperatorFileListElement)
-    directory: StringProperty(subtype='DIR_PATH')
+    directory: StringProperty(subtype='DIR_PATH', default='//')
     filename_ext = ""
+    filter_glob: StringProperty(default='*.jpg;*.png;*.tif;*.tiff;*.bmp')
 
     def execute(self, context):
-        scene = context.scene
-        ddslideshow = scene.ddslideshow
-        fps = context.scene.render.fps
-
-        intro = bpy.path.abspath(ddslideshow.intro)
-        outro = bpy.path.abspath(ddslideshow.outro)
-        slide_duration = int(ddslideshow.slide_duration * fps)
-        slide_crossfade = int(ddslideshow.slide_crossfade * fps)
-
-        files = [os.path.join(self.directory, f.name) for f in self.files]
-
-        if os.path.isfile(intro):
-            files.insert(0, intro)  # noqa: E701
-
-        if os.path.isfile(outro):
-            files.append(outro)  # noqa: E701
-
-        full_duration = slide_duration + 2 * slide_crossfade
-        images = [load_image(bpy.path.abspath(fname), 1 + i * full_duration, full_duration)
-                  for i, fname in enumerate(files)]
-
-        context.scene.frame_end = images[-1].frame_final_end
+        shit_i_cannot_figure_out_how_to_properly_call_load_images__from_create_slideshow(self.files, self.directory, context)
 
         return {'FINISHED'}
 
